d5587444e730337a689b5204a7fa8ca989c665b5
Mihaela Constantin <mihacon67@gmail.com>
2022-08-08 16:11:47 +0300

    second commit

--- index.js
+++ index.js
@@ -1,19 +1,23 @@
-function displayTeams() {
-    console.warn("display", teams);
+function displayTeams(teams) {
+    //console.warn("display", teams);
 
     var teamsHTML = "";
     teams.forEach(function (team) {
-        teamsHTML +=` 
-        <tr>
-        <td>${team.promotion}</td>
-        <td>${team.members}</td>
-        <td${team.name}</td>
-        <td>
-            <a href="${team.url}">open</a>
-        </td>
-        <td>x e</td>
-    </tr>`;
+      //console.info(team.promotion);
+        teamsHTML += ` 
+           <tr>
+                <td>${team.promotion}</td>
+                <td>${team.members}</td>
+                <td>${team.name}</td>
+                <td>
+                  <a href="${team.url}">open</a>
+                </td>
+                <td>x e</td>
+            </tr>`;
     });
+    //console.warn(teamsHTML);
+
+    document.querySelector("table tbody").innerHTML = teamsHTML;
 }
 
 //afisare
@@ -21,13 +25,15 @@ function displayTeams() {
 document.querySelector("table tbody").innerHTML = teamsHTML;
 
 function loadTeams() {
-    fetch("(data/teams.json")
+    fetch("data/teams.json")
       .then(function (r) {
+        //console.info(r);
         return r.json();
       })  
       .then(function (teams) {
+        //console.warn("teams",teams);
         displayTeams(teams);
       });
 }
 
-loadTeams()
+loadTeams();
